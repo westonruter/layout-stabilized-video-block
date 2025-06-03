21a7c4ea1ec0d7303c8a824641ef0234abc09e4c
Weston Ruter <westonruter@gmail.com>
2025-06-02 17:22:22 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Layout-stabilized Video Block #
+
+Contributors: westonruter  
+Tested up to: 6.8  
+Stable tag:   0.1.0  
+License:      [GPLv2](https://www.gnu.org/licenses/gpl-2.0.html) or later  
+Tags:         performance
+
+## Description ##
+
+This plugin adds missing `width` and `height` attributes to the `video` tag in the Video block along with the `aspect-ratio` style to prevent a layout shift when the video is loaded. This improves the Cumulative Layout Shift (CLS) metric from Core Web Vitals.
+
+This only applies to videos selected from the Media Library since only these would have the metadata for the dimensions available.
+
+## Installation ##
+
+1. Download the plugin [ZIP from GitHub](https://github.com/westonruter/layout-stabilized-video-block/archive/refs/heads/main.zip) or if you have a local clone of the repo, run `npm run plugin-zip`.
+2. Visit **Plugins > Add New Plugin** in the WordPress Admin.
+3. Click **Upload Plugin**.
+4. Select the `layout-stabilized-video-block.zip` file on your system from step 1 and click **Install Now**.
+5. Click the **Activate Plugin** button.
+
+You may also install and update via [Git Updater](https://git-updater.com/).
+
+## Changelog ##
+
+### 0.1.0 ###
+
+* Initial release.
---
+++ layout-stabilized-video-block.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Plugin Name: Layout-stabilized Video Block
+ * Plugin URI: https://github.com/westonruter/layout-stabilized-video-block
+ * Description: Adds missing <code>width</code> and <code>height</code> attributes to the <code>video</code> tag in the Video block along with the <code>aspect-ratio</code> style to prevent a layout shift when the video is loaded. Only applies to videos selected from the Media Library. Improves the Cumulative Layout Shift (CLS) metric from Core Web Vitals.
+ * Requires at least: 6.8
+ * Requires PHP: 8.1
+ * Version: 0.1.0
+ * Author: Weston Ruter
+ * Author URI: https://weston.ruter.net/
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Update URI: https://github.com/westonruter/layout-stabilized-video-block
+ * GitHub Plugin URI: https://github.com/westonruter/layout-stabilized-video-block
+ * Primary Branch: main
+ *
+ * @package LayoutStabilizedVideoBlock
+ */
+
+namespace LayoutStabilizedVideoBlock;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // @codeCoverageIgnore
+}
+
+// Short-circuit functionality to facilitate benchmarking performance impact.
+if ( isset( $_GET['disable_layout_stabilized_video_block'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+	return;
+}
+
+use WP_HTML_Tag_Processor;
+
+/**
+ * Filters the content of the video block.
+ *
+ * @since 0.1.0
+ *
+ * @phpstan-param array{
+ *                    "attrs": array{
+ *                        "id"?: positive-int,
+ *                    }
+ *                } $block
+ *
+ * @param string|mixed $content The block content.
+ * @param array        $block   The full block, including name and attributes.
+ */
+function filter_rendered_video_block_content( mixed $content, array $block ): mixed {
+	if ( is_string( $content ) ) {
+		$content = render_block_core_video( $block['attrs'], $content );
+	}
+	return $content;
+}
+add_filter( 'render_block_core/video', filter_rendered_video_block_content( ... ), 10, 2 );
+
+/**
+ * Adds inline style for the Video blocks that have a VIDEO tag with a width and height attribute.
+ *
+ * @since 0.1.0
+ */
+function add_video_block_inline_style(): void {
+	$handle = wp_should_load_separate_core_block_assets() ? 'wp-block-post-featured-image' : 'wp-block-library';
+	wp_add_inline_style( $handle, '.wp-block-video video[width][height]{height:auto}' );
+}
+add_action( 'enqueue_block_assets', add_video_block_inline_style( ... ) );
+
+/**
+ * Renders the `core/video` block on the server to supply the width and height attributes from the attachment metadata.
+ *
+ * @since 0.1.0
+ *
+ * @phpstan-param array{ "id"?: positive-int } $attributes
+ *
+ * @param array  $attributes The block attributes.
+ * @param string $content    The block content.
+ * @return string The block content with the dimensions added.
+ */
+function render_block_core_video( array $attributes, string $content ): string {
+	// if the content lacks any video tag, abort.
+	if ( ! str_contains( $content, '<video' ) ) {
+		return $content;
+	}
+
+	// If the 'id' attribute is not populated for a video attachment, abort.
+	if (
+		! isset( $attributes['id'] ) ||
+		! is_int( $attributes['id'] ) ||
+		$attributes['id'] <= 0
+	) {
+		return $content;
+	}
+
+	// If the 'id' attribute wasn't for an attachment, abort.
+	if ( get_post_type( $attributes['id'] ) !== 'attachment' ) {
+		return $content;
+	}
+
+	// Get the width and height metadata for the video, and abort if absent or invalid.
+	$metadata = wp_get_attachment_metadata( $attributes['id'] );
+	if (
+		! isset( $metadata['width'], $metadata['height'] ) ||
+		! ( is_int( $metadata['width'] ) && is_int( $metadata['height'] ) ) ||
+		! ( $metadata['width'] > 0 && $metadata['height'] > 0 )
+	) {
+		return $content;
+	}
+
+	// Locate the VIDEO tag to add the dimensions.
+	$p = new WP_HTML_Tag_Processor( $content );
+	if ( ! $p->next_tag( array( 'tag_name' => 'VIDEO' ) ) ) {
+		return $content;
+	}
+
+	$p->set_attribute( 'width', (string) $metadata['width'] );
+	$p->set_attribute( 'height', (string) $metadata['height'] );
+
+	$style = $p->get_attribute( 'style' );
+	if ( ! is_string( $style ) ) {
+		$style = '';
+	}
+	$aspect_ratio_style = sprintf( 'aspect-ratio: %d / %d;', $metadata['width'], $metadata['height'] );
+	$p->set_attribute( 'style', $aspect_ratio_style . $style );
+
+	return $p->get_updated_html();
+}
